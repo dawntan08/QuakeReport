2c15cfb19d5f68cd1ac1236ab58a62a45493c98f
Dawn <dawntan08@gmail.com>
2017-08-29 13:59:44 +1000

    Add oval drawabale for background of magnitude.  + copy color values for diff magnitudes to color.xml  + modify logic in adapter to show proper color for each mag

--- app/src/main/java/com/example/android/quakereport/EarthquakeInfoAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeInfoAdapter.java
@@ -1,6 +1,8 @@
 package com.example.android.quakereport;
 
 import android.app.Activity;
+import android.graphics.drawable.GradientDrawable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,7 @@ import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+
 
 /**
  * Created by liway on 24/08/2017.
@@ -44,6 +47,17 @@ public class EarthquakeInfoAdapter extends ArrayAdapter<Earthquake> {
         // Find the TextView in the xml layout with the ID magTextView, and set its value
         TextView magTextView = (TextView) listItemView.findViewById(R.id.magnitude);
         magTextView.setText(formatMagnitude(currentEquakeInfo.getMag()));
+
+        // Set the proper background color on the magnitude circle.
+        // Fetch the background from the TextView, which is a GradientDrawable.
+        GradientDrawable magnitudeCircle = (GradientDrawable) magTextView.getBackground();
+
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getMagnitudeColor(currentEquakeInfo.getMag());
+
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
+
 
         // Find the TextView in the xml layout with the ID cityTextView, and set its value
         TextView cityTextView = (TextView) listItemView.findViewById(R.id.location);
@@ -84,6 +98,47 @@ public class EarthquakeInfoAdapter extends ArrayAdapter<Earthquake> {
         return magnitudeFormat.format(magnitude);
     }
 
+    /**
+     * Return the formatted magnitude string showing 1 decimal place (i.e. "3.2")
+     * from a decimal magnitude value.
+     */
+    private int getMagnitudeColor(double magnitude) {
+        int magnitudeColor;
+        switch ((int)Math.floor(magnitude)){
+            case 0:
+            case 1:
+                magnitudeColor = R.color.magnitude1;
+                break;
+            case 2:
+                magnitudeColor = R.color.magnitude2;
+                break;
+            case 3:
+                magnitudeColor = R.color.magnitude3;
+                break;
+            case 4:
+                magnitudeColor = R.color.magnitude4;
+                break;
+            case 5:
+                magnitudeColor = R.color.magnitude5;
+                break;
+            case 6:
+                magnitudeColor = R.color.magnitude6;
+                break;
+            case 7:
+                magnitudeColor = R.color.magnitude7;
+                break;
+            case 8:
+                magnitudeColor = R.color.magnitude8;
+                break;
+            case 9:
+                magnitudeColor = R.color.magnitude9;
+                break;
+            default:
+                magnitudeColor = R.color.magnitude10plus;
+                break;
+        }
+        return ContextCompat.getColor(getContext(), magnitudeColor);
+    }
     /**
      * Return the formatted date string (i.e. "Mar 3, 1984") from a Date object.
      */
